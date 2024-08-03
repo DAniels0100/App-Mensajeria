66e5710d67ffc8a9342fb025ae3e38d338736453
Daniels <se.alchanto@gmail.com>
2024-08-02 18:14:11 -0600

    second commit

--- ChatApp/Program.cs
+++ ChatApp/Program.cs
@@ -12,9 +12,6 @@ namespace test
         {
             Chat chat = new Chat();
 
-            //Console.Write("Do you want to host a chat? (y/n): ");
-            //string choice = Console.ReadLine();
-
             try
             {
                 chat.Connect("127.0.0.1", 8000);
@@ -25,17 +22,6 @@ namespace test
                 chat.StartListening(8000);
                 Console.WriteLine("you are the host");
             }
-
-            /*
-             *
-            if (choice.ToLower() == "y")
-            {
-                chat.StartListening(8000);
-            }
-            else
-            {
-                chat.Connect("127.0.0.1", 8000);
-            }*/
 
             while (true)
             {
